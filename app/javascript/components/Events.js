87ad6df3938702699b4f1ad32ea49f4c610a3a4d
Frank Lee <franklee@umail.ucsb.edu>
2019-11-07 15:00:08 -0800

    added cleaner date parsing

--- app/javascript/components/Events.js
+++ app/javascript/components/Events.js
@@ -3,13 +3,21 @@ import React from "react";
 const Events = props => {
   return (
     <div>
-      {props.events.length
-        ? <h1 align="center">Calendar events for {props.events[0].organizer.display_name}:</h1>
-        : "No events in this calendar"}
+      {props.events.length ? (
+        <h1 align="center">
+          Calendar events for {props.events[0].organizer.display_name}:
+        </h1>
+      ) : (
+        "No events in this calendar"
+      )}
       <ul>
         {props.events.map(event => (
           <li key={event.id}>
-            {event.summary} starting at {(new Date(Date.parse(event.end.date_time))).toString()}
+            {event.summary} starting at{" "}
+            {new Date(Date.parse(event.end.date_time)).toLocaleDateString(
+              "en-US",
+              { weekday: "long", hour: "2-digit", minute: "2-digit" }
+            )}
           </li>
         ))}
       </ul>
