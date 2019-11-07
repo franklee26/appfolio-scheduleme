ef720d5325c253ac3dd24d4ef6bcd63b516e2dfd
Frank Lee <franklee@umail.ucsb.edu>
2019-11-07 15:31:29 -0800

    organised routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   get 'calendar/index'
 
+  # home page
   root 'homepage#index'
+
+  # about page
   get 'about' => 'about#index'
+
+  # authentication page calls google oauth
   get 'auth' => 'calendar#auth'
+
+  # calendar calls
   get 'calendar', to: 'calendar#index'
   get 'calendar/auth', to: 'calendar#auth'
   get 'calendar/callback', to: 'calendar#callback'
