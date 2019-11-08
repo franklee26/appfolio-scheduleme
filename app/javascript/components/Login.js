982678145b54bc850ef645404bebb6911595aebc
Edward Yuen <edwardyuensf@gmail.com>
2019-11-07 16:34:04 -0800

    fleshed out tenant model, tenant tests, and added tenant Login page

--- app/javascript/components/Login.js
+++ app/javascript/components/Login.js
@@ -11,10 +11,10 @@ import {
 const Login = () => {
   return (
   <div class="container center_div col-xs-4">
-      This is the Login page!
+      <h1 align="center">Tenant Login Page</h1>
         <Form>
         <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label>Email Address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
             We'll never share your email with anyone else.
@@ -26,10 +26,23 @@ const Login = () => {
             <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+            <Form.Label>I'm a: &ensp; </Form.Label>
+            
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                <label class="form-check-label" for="inlineRadio1">Tenant</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                <label class="form-check-label" for="inlineRadio2">Landlord</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"/>
+                <label class="form-check-label" for="inlineRadio3">Vendor</label>
+            </div>
         </Form.Group>
         <Button variant="primary" type="submit">
-            Submit
+            Log In
         </Button>
         </Form>
   </div>
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'calendar/index'
-=======
   get 'tenants/new'
-<<<<<<< HEAD
->>>>>>> added email to tenant, and validation tests
-=======
-  get 'tenants/login'
->>>>>>> added login view and login JS component for tenants
 
   root 'homepage#index'
   get 'about' => 'about#index'
