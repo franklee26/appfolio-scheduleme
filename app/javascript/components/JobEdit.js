acc200c6b9d45189eb37efb8823dc904e0a859f4
Raulido <Raulido@outlook.com>
2019-11-11 03:21:37 -0800

    added react component for edit/form

---
+++ app/javascript/components/JobEdit.js
@@ -0,0 +1,47 @@
+import React from "react";
+class JobEdit extends React.Component {
+  render() {
+    var errors = null;
+      if(this.props.errors.length > 0) {
+        var errorsList = [];
+        for(var i = 0; i < this.props.errors.length; ++i) {
+          errorsList.push(<li key={"error-" + i}>{this.props.errors[i]}</li>)
+        }
+  
+        errors = (
+          <div id="error_explanation">
+            <h2>Errors prohibited this job from being saved:</h2>
+            <ul>
+              {errorsList}
+            </ul>
+          </div>
+        );
+      }
+        return (
+            <div>
+              <h1>Editing Job</h1>
+              <form action={this.props.form_path} method="post">
+          {errors}
+          <input type="hidden" name="_method" defaultValue={this.props.form_method} />
+          <input type="hidden" name="authenticity_token" defaultValue={this.props.csrf_token} />
+          <input type="hidden" name="UTF-8" defaultValue="✓" />
+          <div className="field">
+            <label htmlFor="content">Content:</label>
+            <input type="text" id="title" name="job[content]" defaultValue={this.props.content} />
+          </div>
+          <div className="field">
+            <label htmlFor="Tenant_id">Tenant_id:</label>
+            <input type="number" id="price" name="job[tenant_id]" defaultValue={this.props.Tenant_id} />
+          </div>
+          <div className="actions">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+              
+              <a href={this.props.show_path}>Show</a> |
+              <a href={this.props.back_path}>Back</a>
+            </div>
+        );
+    };
+}
+export default JobEdit;
