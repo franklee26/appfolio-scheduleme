a7318305e53910464418bb0ee974c26e24749cdf
Raulido <Raulido@outlook.com>
2019-11-10 21:16:15 -0800

    Added a Job scaffold with association relationship to tenant, included examples in seeds.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+    belongs_to :Tenant
+    validates :content, length: { maximum: 140 },
+        presence: true
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,4 +1,5 @@
 class Tenant < ApplicationRecord
+    has_many :jobs
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive:false }
