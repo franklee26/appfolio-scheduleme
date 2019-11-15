6226bc182dbef46f8dd526e446d44fde2e60c033
Edward Yuen <edwardyuensf@gmail.com>
2019-11-14 22:33:26 -0800

    created a sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+  end
+  def destroy
+  end
+  def login
+  end
+  def landing_page
+  end
+  def profile_page
+  end
+end
