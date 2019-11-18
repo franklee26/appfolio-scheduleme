b7f353d542b41b02dbd9e9347a1de0d203f19014
Raulido <Raulido@outlook.com>
2019-11-18 10:26:58 -0800

    Added Vendor_id to Job Model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
     belongs_to :Tenant
+    belongs_to :Vendor
     validates :content, length: { maximum: 140 },
         presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191114122751) do
+ActiveRecord::Schema.define(version: 20191118165159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20191114122751) do
     t.integer "Tenant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "Vendor_id"
   end
 
   create_table "landowners", force: :cascade do |t|
