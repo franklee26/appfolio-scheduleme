724ab06fa2172a789c87e3e32644e53204126341
Frank Lee <franklee@umail.ucsb.edu>
2020-01-18 21:32:52 -0800

    changed from class to hooks

--- app/javascript/components/Events.js
+++ app/javascript/components/Events.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // handle click and post request
 const handleClickPost = (
@@ -39,111 +39,100 @@ const shortFormatDate = date => {
   return theDate;
 };
 
-class Events extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      calendarResponse: null
-    };
-  }
+const Events = props => {
+  const [state, setState] = useState({
+    error: null,
+    isLoaded: false,
+    calendarResponse: null
+  });
 
-componentDidMount() {
-    fetch(`http://localhost:3000/calendar/${this.props.calendar_id}/response`, {
+  useEffect(() => {
+    fetch(`http://localhost:3000/calendar/${props.calendar_id}/response`, {
       method: "GET"
     })
       .then(res => res.json())
       .then(
         result => {
-          this.setState({
-            isLoaded: true,
-            calendarResponse: result
-          });
+          setState({ ...state, isLoaded: true, calendarResponse: result });
         },
         error => {
-          this.setState({
-            isLoaded: true,
-            error: error
-          });
+          setState({ ...state, isLaoded: true, error: error });
         }
       );
-  }
+  }, []);
 
-  render() {
-    const { error, isLoaded, calendarResponse } = this.state;
-    if (error) {
-      return <div>Error in mount: {error.message}</div>;
-    } else if (!isLoaded) {
-      return (
-        <div>
-          <h1>Loading and scheduling times...</h1>
-        </div>
-      );
-    } else if (calendarResponse["error"]) {
-      return (
-        <div>
-          <h1>
-            Error code {calendarResponse["error"]["code"]} in API call:{" "}
-            {calendarResponse["error"]["message"]}
-          </h1>
-        </div>
-      );
-    }
+  const { error, isLoaded, calendarResponse } = state;
+  if (error) {
+    return <div>Error in mount: {error.message}</div>;
+  } else if (!isLoaded) {
     return (
-      <div className="container">
-        <h1 align="center">Selected Calendar: {calendarResponse["summary"]}</h1>
-        <h4>
-          {" "}
-          Submit your free times (pulled from the last two weeks from everyone's
-          calendars):{" "}
-        </h4>
-        {this.props.free_times.map(timeHash => (
-          <table>
-            <td>
-              <a
-                href="#"
-                onClick={e => {
-                  handleClickPost(
-                    e,
-                    timeHash["start"],
-                    timeHash["end"],
-                    this.props.calendar_id,
-                    calendarResponse["summary"]
-                  );
-                }}
-              >
-                From {shortFormatDate(timeHash["start"])} to{" "}
-                {shortFormatDate(timeHash["end"])}{" "}
-              </a>
-            </td>
-          </table>
-        ))}
-        {this.props.events.length ? (
-          <h1 align="center">
-            Calendar events for {calendarResponse["summary"]}:
-          </h1>
-        ) : (
-          `No events in ${calendarResponse["summary"]} calendar`
-        )}
-        <ul>
-          {this.props.events.map(event => (
-            <li key={event.id}>
-              {event.summary} starting at{" "}
-              {new Date(Date.parse(event.end.date_time)).toLocaleDateString(
-                "en-US",
-                {
-                  weekday: "long",
-                  hour: "2-digit",
-                  minute: "2-digit"
-                }
-              )}
-            </li>
-          ))}
-        </ul>
+      <div>
+        <h1>Loading and scheduling times...</h1>
+      </div>
+    );
+  } else if (calendarResponse["error"]) {
+    return (
+      <div>
+        <h1>
+          Error code {calendarResponse["error"]["code"]} in API call:{" "}
+          {calendarResponse["error"]["message"]}
+        </h1>
       </div>
     );
   }
-}
+  return (
+    <div className="container">
+      <h1 align="center">Selected Calendar: {calendarResponse["summary"]}</h1>
+      <h4>
+        {" "}
+        Submit your free times (pulled from the last two weeks from everyone's
+        calendars):{" "}
+      </h4>
+      {props.free_times.map(timeHash => (
+        <table>
+          <td>
+            <a
+              href="#"
+              onClick={e => {
+                handleClickPost(
+                  e,
+                  timeHash["start"],
+                  timeHash["end"],
+                  props.calendar_id,
+                  calendarResponse["summary"]
+                );
+              }}
+            >
+              From {shortFormatDate(timeHash["start"])} to{" "}
+              {shortFormatDate(timeHash["end"])}{" "}
+            </a>
+          </td>
+        </table>
+      ))}
+      {props.events.length ? (
+        <h1 align="center">
+          Calendar events for {calendarResponse["summary"]}:
+        </h1>
+      ) : (
+        `No events in ${calendarResponse["summary"]} calendar`
+      )}
+      <ul>
+        {props.events.map(event => (
+          <li key={event.id}>
+            {event.summary} starting at{" "}
+            {new Date(Date.parse(event.end.date_time)).toLocaleDateString(
+              "en-US",
+              {
+                weekday: "long",
+                hour: "2-digit",
+                minute: "2-digit"
+              }
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Events;
