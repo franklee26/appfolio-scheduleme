90f10e207fec2941756a871ca60c14a5c9f7fd83
Eric Shen <ererics41@gmail.com>
2020-01-19 21:54:23 -0800

    made tenant profile fileds editable

--- app/javascript/components/TenantProfilePage.js
+++ app/javascript/components/TenantProfilePage.js
@@ -5,22 +5,66 @@ class TenantProfilePage extends Component {
     super(props);
     this.state = {
       tenant_id: this.props.id,
-      tenant_info: {"name": "", "email": "", "landowner_id": ""}
+      name: "", 
+      email: "", 
+      landowner_id: "",
+      changed: false
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch("/tenants/" + this.state.tenant_id)
       .then((response) => {return response.json()})
-      .then((data) => {this.setState({ tenant_info: data})});
+      .then((data) => {this.setState({ name: data.name, email: data.email, landowner_id: data.landowner_id})});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // Todo: change the values in the database
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+      changed: true
+    });
   }
 
   render() {
     return (
       <div>
-        <div>{this.state.tenant_info.name}</div>
-        <div>{this.state.tenant_info.email}</div>
-        <div>{this.state.tenant_info.landowner_id}</div>
+        <h1>Tenant Profile</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Name</label>
+            <input
+              value={this.state.name}
+              name="name"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              value={this.state.email}
+              name="email"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div>
+            <label>Landlord ID</label>
+            <input
+              value={this.state.landowner_id}
+              name="landownder_id"
+              onChange={e => this.handleChange(e)}
+            />
+          </div>
+          <div>
+            {this.state.changed && <button onSubmit={e => this.handleSubmit(e)}>Save</button>}
+          </div>
+        </form>
       </div>
     );
   }
