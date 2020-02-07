2dbbd5b876162af03b57a58275e64fc8c64f206c
Frank Lee <franklee@umail.ucsb.edu>
2020-02-06 21:17:06 -0800

    refactoring

--- app/javascript/components/JobNew.js
+++ app/javascript/components/JobNew.js
@@ -1,52 +1,5 @@
 import React from "react";
 
-const handleSubmit = (event, landowner_id, tenant_id, state) => {
-  if (landowner_id === 0) {
-    alert("Can't schedule job yet! (You have no landowner yet.)");
-    return;
-  }
-  fetch(
-    `http://localhost:3000/calendar/schedule/${landowner_id}/${tenant_id}`,
-    {
-      method: "GET"
-    }
-  )
-    .then(response => response.json())
-    .then(response => {
-      if (response.vendors.length === 0) {
-        alert("Can't schedule job yet! (Can not find any free times)");
-        throw new Error("No free times found...");
-      } else {
-        return response;
-      }
-    })
-    .then(response =>
-      response.vendors.map(v =>
-        fetch("http://localhost:3000/jobs/new_temp_job", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            content: state.content,
-            title: state.title,
-            job_type: state.job_type,
-            status: "LANDOWNER APPROVED",
-            tenant_id: tenant_id,
-            start: v.start,
-            end: v.end,
-            vendor_id: v.vendor.id
-          })
-        })
-      )
-    )
-    .then(res =>
-      alert(
-        "Found available times! Confirm job by adding job to your calendar."
-      )
-    ).then(res => document.getElementById("jobForm").submit())
-    .catch(error => console.log(error));
-    event.preventDefault()
-};
-
 class JobNew extends React.Component {
   constructor(props) {
     super(props);
@@ -55,12 +8,60 @@ class JobNew extends React.Component {
       title: "manual title",
       job_type: "some type"
     }
-    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleTitleChange(e) {
+  handleContentChange(e) {
     this.setState({ content: e.target.value })
   }
+
+  handleSubmit(event) {
+    if (this.props.landowner_id_call === 0) {
+      alert("Can't schedule job yet! (You have no landowner yet.)");
+      return;
+    }
+    fetch(
+      `http://localhost:3000/calendar/schedule/${this.props.landowner_id_call}/${this.props.tenant_id_call}`,
+      {
+        method: "GET"
+      }
+    )
+      .then(response => response.json())
+      .then(response => {
+        if (response.vendors.length === 0) {
+          alert("Can't schedule job yet! (Can not find any free times)");
+          throw new Error("No free times found...");
+        } else {
+          return response;
+        }
+      })
+      .then(response =>
+        response.vendors.map(v =>
+          fetch("http://localhost:3000/jobs/new_temp_job", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              content: this.state.content,
+              title: this.state.title,
+              job_type: this.state.job_type,
+              status: "LANDOWNER APPROVED",
+              tenant_id: this.props.tenant_id_call,
+              start: v.start,
+              end: v.end,
+              vendor_id: v.vendor.id
+            })
+          })
+        )
+      )
+      .then(res =>
+        alert(
+          "Found available times! Confirm job by adding job to your calendar."
+        )
+      ).then(res => document.getElementById("jobForm").submit())
+      .catch(error => console.log(error));
+      event.preventDefault()
+  };
 
   render() {
     var errors = null;
@@ -104,11 +105,11 @@ class JobNew extends React.Component {
             id="title"
             name="job[content]"
             defaultValue={this.props.content}
-            onChange={this.handleTitleChange}
+            onChange={this.handleContentChange}
           />
         </div>
         <div className="actions">
-          <button type="submit" onClick={e => handleSubmit(e, this.props.landowner_id_call, this.props.tenant_id_call, this.state)}>
+          <button type="submit" onClick={e => this.handleSubmit(e)}>
             Submit
           </button>
           <a href={"http://localhost:3000/calendar"}>Back</a>
