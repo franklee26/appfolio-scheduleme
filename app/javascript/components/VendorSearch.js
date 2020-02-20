63f4e137ea365d6bb1e0955e2de0290c1924d425
Frank Lee <franklee@umail.ucsb.edu>
2020-02-19 21:54:23 -0800

    linting

--- app/javascript/components/VendorSearch.js
+++ app/javascript/components/VendorSearch.js
@@ -23,13 +23,18 @@ class VendorSearch extends Component {
     // Todo: Validate the entered search parameters
     // Todo: if valid, pass the information in the state to the controller and redirect
     var vendors = this.props.vendors;
-    vendors = vendors.filter((vendor) => {
-      let occupationCheck = this.state.occupation.length == 0 || this.state.occupation.toUpperCase() == vendor.occupation.toUpperCase();
-      let nameCheck = this.state.name.length == 0 || (vendor.name.toUpperCase().indexOf(this.state.name.toUpperCase()) != -1);
-      let zipCheck = this.state.zip.length == 0 || (vendor.zip == this.state.zip);
-      let notDefault = vendor.name != "DEFAULT" && vendor.occupation != "DEFAULT";
+    vendors = vendors.filter(vendor => {
+      let occupationCheck =
+        this.state.occupation.length == 0 ||
+        this.state.occupation.toUpperCase() == vendor.occupation.toUpperCase();
+      let nameCheck =
+        this.state.name.length == 0 ||
+        vendor.name.toUpperCase().indexOf(this.state.name.toUpperCase()) != -1;
+      let zipCheck = this.state.zip.length == 0 || vendor.zip == this.state.zip;
+      let notDefault =
+        vendor.name != "DEFAULT" && vendor.occupation != "DEFAULT";
       return occupationCheck && nameCheck && zipCheck && notDefault;
-    })
+    });
     this.setState({
       submitted: true,
       list: vendors
@@ -51,78 +56,89 @@ class VendorSearch extends Component {
   }
 
   render() {
-    if(this.state.vendorSelected) {
+    if (this.state.vendorSelected) {
       window.location.href = `/vendors/display/${this.state.selectedVendor.id}`;
-      return (<div> </div>);
+      return <div> </div>;
     } else {
       return (
         <div>
           <div>
-            <header className = "bg-dark py-3">
-            <h1 align="center" className="display-3 text-white mt-5 mb-2">Find a Vendor</h1>
+            <header className="bg-dark py-3">
+              <h1 align="center" className="display-3 text-white mt-5 mb-2">
+                Find a Vendor
+              </h1>
             </header>
           </div>
           <div className="container">
-          <form align = "center" onSubmit={this.handleSubmit}>
-            <div>
-              <label>Search by Name</label>
-              <input
-                value={this.state.name}
-                name="name"
-                onChange={e => this.handleChange(e)}
-              />
-            </div>
-            <div>
-              <label>Filter by Occupation</label>
-              <input
-                value={this.state.occupation}
-                name="occupation"
-                onChange={e => this.handleChange(e)}
-              />
-            </div>
-            <div>
-              <label>Filter by Zip Code</label>
-              <input
-                value={this.state.zip}
-                name="zip"
-                onChange={e => this.handleChange(e)}
-              />
-            </div>
-            <div>
-              <Button 
-                variant="primary"
-                type="submit" 
-                onClick={e => this.handleSubmit(e)}
-              >
-                search
-              </Button>
-            </div>
-          </form>
-          <div>
-            {this.state.submitted &&
+            <form align="center" onSubmit={this.handleSubmit}>
               <div>
-                <h1 align="center">Search Results</h1>
-                {this.state.list.length ? (
-                  this.state.list.map(vendor => (
-                    <Button variant="secondary" size="lg" block onClick={e => this.handleVendorListClick(e,vendor)}>
-                      Name: {vendor.name}    Ocupation: {vendor.occupation}                     ({
-                      <StarRatings
-                        rating={parseFloat(vendor.rating ? vendor.rating.toFixed(2) : "0.0")}
-                        starDimension="19px"
-                        starSpacing="1px"
-                        starRatedColor="gold"
-                        numberOfStars={5}
-                        name="rating"
-                      />
-                    })
-                    </Button>
-                  ))
-                ) : (
-                  <label> No results found. </label>
-                )}
+                <label>Search by Name</label>
+                <input
+                  value={this.state.name}
+                  name="name"
+                  onChange={e => this.handleChange(e)}
+                />
               </div>
-            }
-          </div>
+              <div>
+                <label>Filter by Occupation</label>
+                <input
+                  value={this.state.occupation}
+                  name="occupation"
+                  onChange={e => this.handleChange(e)}
+                />
+              </div>
+              <div>
+                <label>Filter by Zip Code</label>
+                <input
+                  value={this.state.zip}
+                  name="zip"
+                  onChange={e => this.handleChange(e)}
+                />
+              </div>
+              <div>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={e => this.handleSubmit(e)}
+                >
+                  search
+                </Button>
+              </div>
+            </form>
+            <div>
+              {this.state.submitted && (
+                <div>
+                  <h1 align="center">Search Results</h1>
+                  {this.state.list.length ? (
+                    this.state.list.map(vendor => (
+                      <Button
+                        variant="secondary"
+                        size="lg"
+                        block
+                        onClick={e => this.handleVendorListClick(e, vendor)}
+                      >
+                        Name: {vendor.name} Ocupation: {vendor.occupation} (
+                        {
+                          <StarRatings
+                            rating={parseFloat(
+                              vendor.rating ? vendor.rating.toFixed(2) : "0.0"
+                            )}
+                            starDimension="19px"
+                            starSpacing="1px"
+                            starRatedColor="gold"
+                            numberOfStars={5}
+                            name="rating"
+                          />
+                        }
+                        )
+                      </Button>
+                    ))
+                  ) : (
+                    <label> No results found. </label>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       );
@@ -130,4 +146,4 @@ class VendorSearch extends Component {
   }
 }
 
-export default VendorSearch
+export default VendorSearch;
